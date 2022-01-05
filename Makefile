de1e1be27cb9d7a8b57facffb7dbdfe361d0925d
sven-eliasen <jo.pettersen@pm.me>
2022-01-05 10:21:45 +0100

    init
    Co-authored-by: arthurd2000@gmail.com <arthurd2000@gmail.com>

---
+++ Makefile
@@ -0,0 +1,4 @@
+SRC_NAME = main
+
+all:
+	nasm -f elf32 $(SRC_NAME).asm && ld -m elf_i386 $(SRC_NAME).o && ./a.out
---
+++ main.asm
@@ -0,0 +1,10 @@
+section .text
+    global _start
+
+_start:
+    
+
+end:
+    mov eax, 1
+    mov ebx, 0
+    int 80h
